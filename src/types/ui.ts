cd34edef62016dc2233aa1f997385e2df720efe3
menukahansda <menukahansda199@gmail.com>
2026-06-26 01:24:27 +0530

    refactor: update chip color types

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,6 +1,6 @@
 // ChipProps
 
-export type ChipColor = "grey" | "green" | "yellow" | "red" | "blue";
+export type ChipColor = "slate" | "emerald" | "yellow" | "red" | "blue";
 // rgy - level colors, blue - person color(manager, hr, etc), grey - normal chips
 
 export type ChipProps = {

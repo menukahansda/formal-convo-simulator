07c3c9a2ba537d28968099b0582f103d81981bc0
menukahansda <menukahansda199@gmail.com>
2026-06-26 02:46:13 +0530

    feat: add scenario card to the practice page

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 import { useState } from "react";
+import ScenarioCard from "@/components/shared/ScenarioCard";
+import type { LLMResponse } from "@/types/llm";
+
 export default function Practice() {
   const [searchTopic, setSearchTopic] = useState("");
+  const scenarios : LLMResponse[] = [
+  {
+    topic: "Team Conflict",
+    scenario: "You are working under a strict team leader in a high-pressure project.",
+    difficulty: "Easy",
+    person: "Team Leader",
+    tone: "Formal"
+  },
+  {
+    topic: "Client Meeting",
+    scenario: "You are handling a dissatisfied client who wants immediate resolution.",
+    difficulty: "Hard",
+    person: "Client",
+    tone: "Professional"
+  },
+];
   async function handleFormSubmit(e: React.SubmitEvent) {
     e.preventDefault();
     console.log(`form submitted with topic = ${searchTopic}`);
+    // send to llm and fetch data - data type -
   }
   return (
     <>
@@ -19,7 +39,11 @@ export default function Practice() {
             onChange={(e) => setSearchTopic(e.target.value)}
           />
         </form>
-        
+        <div className="mt-6 flex flex-col gap-4 items-center">
+          {scenarios.map((sc, idx)=>(
+            <ScenarioCard key={idx} topic={sc.topic} scenario={sc.scenario} difficulty={sc.difficulty} person={sc.person} tone={sc.tone}/>
+          ))}
+        </div>
       </div>
     </>
   );
